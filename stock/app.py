947393c2ecca4eff9e885a74ef547547fd3fc284
MWschutte <marcus.schutte@live.nl>
2022-05-12 14:36:29 +0200

    Add basic CRUD for stock service

--- stock/app.py
+++ stock/app.py
@@ -3,7 +3,9 @@ import atexit
 
 from flask import Flask
 import redis
+from stock import Stock
 
+import json
 
 app = Flask("stock-service")
 
@@ -22,19 +24,51 @@ atexit.register(close_db_connection)
 
 @app.post('/item/create/<price>')
 def create_item(price: int):
-    pass
+    """
+    Create stock item with given price
+    :param price:
+    :return: Stock
+    """
+    s = Stock(price)
+    db.set(s.id, s.dumps())
+    return s.dumps()
 
 
 @app.get('/find/<item_id>')
 def find_item(item_id: str):
-    pass
+    """
+    Get item if it exists
+    :param item_id:
+    :return: Stock
+    """
+    return db.get(item_id)
 
 
 @app.post('/add/<item_id>/<amount>')
 def add_stock(item_id: str, amount: int):
-    pass
+    """
+    Add amount to stock
+    :param item_id:
+    :param amount:
+    :return: Stock
+    """
+    d = db.get(item_id)
+    s = Stock.loads(d)
+    s.stock += int(amount)
+    db.set(s.id, s.dumps())
+    return s.dumps()
 
 
 @app.post('/subtract/<item_id>/<amount>')
 def remove_stock(item_id: str, amount: int):
-    pass
+    """
+    Remove stock from item with given id.
+    :param item_id: The item to subtract amount from.
+    :param amount: The amount to remove from the stock.
+    :return: Stock item.
+    """
+    d = db.get(item_id)
+    s = Stock.loads(d)
+    s.stock -= int(amount)
+    db.set(s.id, s.dumps())
+    return s.dumps()
