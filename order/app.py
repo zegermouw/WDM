92ceda7f4555d5726d6ee717e8d1e6cea4297801
timpelser <t.f.pelser@student.tudelft.nl>
2022-05-13 15:01:56 +0200

    Fixed Add, find and delete methods

--- order/app.py
+++ order/app.py
@@ -1,15 +1,17 @@
 import os
 import atexit
+import json
 from pymongo import MongoClient
-
 from flask import Flask
+from bson import json_util
+from bson.objectid import ObjectId
 
 gateway_url = os.environ['GATEWAY_URL']
 
 app = Flask("order-service")
 
-client = MongoClient('HOST HERE', 'PORT HERE')
-db = client['DATABASE NAME HERE']
+client = MongoClient('mongodb://order-db:27017/db', 27017)
+db = client['local']
 
 
 def close_db_connection():
@@ -20,23 +22,24 @@ atexit.register(close_db_connection)
 
 
 # TODO: handle error handling for when mongodb fails.
+# TODO: Better handle mongodb responses.
 
 @app.post('/create/<user_id>')
 def create_order(user_id):
     order = {'user_id:': user_id}
     order_id = db.orders.insert_one(order).inserted_id
-    return order_id, 200
+    return str(order_id), 200
 
 
 @app.delete('/remove/<order_id>')
 def remove_order(order_id):
-    status = db.orders.delete_one({"_id": order_id})
-    return status, 200
+    status = db.orders.delete_one({"_id": ObjectId(order_id)})
+    return str(status), 200
 
 
 @app.post('/addItem/<order_id>/<item_id>')
 def add_item(order_id, item_id):
-    order = db.orders.find_one({"_id": order_id})
+    order = db.orders.find_one({"_id": ObjectId(order_id)})
     order.setdefault('items', []).append(item_id)
     db.orders.update_one({"_id": order_id}, {"$set": order}, upsert=False)
     return f'Added item: {item_id}', 200
@@ -44,7 +47,7 @@ def add_item(order_id, item_id):
 
 @app.delete('/removeItem/<order_id>/<item_id>')
 def remove_item(order_id, item_id):
-    order = db.orders.find_one({"_id": order_id})
+    order = db.orders.find_one({"_id": ObjectId(order_id)})
     if item_id in order['items']:
         order['items'].remove(order['items'])
         db.orders.update_one({"_id": order_id}, {"$set": order}, upsert=False)
@@ -56,8 +59,8 @@ def remove_item(order_id, item_id):
 
 @app.get('/find/<order_id>')
 def find_order(order_id):
-    order = db.orders.find_one({"_id": order_id})
-    return order, 200
+    order = db.orders.find_one({"_id": ObjectId(order_id)})
+    return str(json.dumps(order, default=json_util.default)), 200
 
 
 @app.post('/checkout/<order_id>')
