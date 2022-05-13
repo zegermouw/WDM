6c680f4799d00957ed4c75050d3c8922268d8268
timpelser <t.f.pelser@student.tudelft.nl>
2022-05-13 15:09:07 +0200

    Fexed add_item + remove_item

--- order/app.py
+++ order/app.py
@@ -22,7 +22,7 @@ atexit.register(close_db_connection)
 
 
 # TODO: handle error handling for when mongodb fails.
-# TODO: Better handle mongodb responses.
+# TODO: Better handle mongodb response format.
 
 @app.post('/create/<user_id>')
 def create_order(user_id):
@@ -41,7 +41,7 @@ def remove_order(order_id):
 def add_item(order_id, item_id):
     order = db.orders.find_one({"_id": ObjectId(order_id)})
     order.setdefault('items', []).append(item_id)
-    db.orders.update_one({"_id": order_id}, {"$set": order}, upsert=False)
+    db.orders.update_one({"_id": ObjectId(order_id)}, {"$set": order}, upsert=False)
     return f'Added item: {item_id}', 200
 
 
@@ -49,8 +49,8 @@ def add_item(order_id, item_id):
 def remove_item(order_id, item_id):
     order = db.orders.find_one({"_id": ObjectId(order_id)})
     if item_id in order['items']:
-        order['items'].remove(order['items'])
-        db.orders.update_one({"_id": order_id}, {"$set": order}, upsert=False)
+        order['items'].remove(item_id)
+        db.orders.update_one({"_id": ObjectId(order_id)}, {"$set": order}, upsert=False)
         return f'Removed item: {item_id}', 200
     else:
         print('this does not exist')
