d34a9f3ff286fa230ef612f15a97f874a4f2b2cc
Sreeparna <sreeparna9324@gmail.com>
2022-05-28 14:08:36 +0200

    added user model

--- payment/models.py
+++ payment/models.py
@@ -1,4 +1,12 @@
+from typing import List
 from pydantic import BaseModel
+from typing import List, Optional
+
+class Order(BaseModel):
+    order_id: str
+    credit_paid: int
+    status: str
 
 class User(BaseModel):
     credit: int = 0
+    payments: Optional[List[Order]]
