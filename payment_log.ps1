f42a57b2b95b199da40f5f6d2375bab3e400c72f
MWschutte <marcus.schutte@live.nl>
2022-06-20 13:15:46 +0200

    add payment log check shell script

---
+++ payment_log.ps1
@@ -0,0 +1,11 @@
+$a = kubectl get pods --all-namespaces | Select-String 'payment'
+$a | foreach-object {
+$_.toString().Split(" ") | where {$_ -like "payment*"
+
+}} | foreach-object { 
+    echo $_  
+    kubectl logs $_ 
+    echo `0
+    echo __________________________________________________________________________________________________________________________
+    echo `0
+}
