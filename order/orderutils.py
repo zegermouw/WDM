24e1c8722cce27f60228029a0375c7c1832449a8
MWschutte <72599545+MWschutte@users.noreply.github.com>
2022-06-21 13:13:31 +0200

    improve listing of other replicas (#14)

--- order/orderutils.py
+++ order/orderutils.py
@@ -2,8 +2,6 @@ import requests
 from requests import Response
 
 # TODO: will change in Kubernetes
-ORDER_URL = STOCK_URL = PAYMENT_URL = COORDINATOR_URL = "http://host.docker.internal:8000"
-ORDER_SERVICE = "http://order-service:5000"
 COORDINATOR_SERVICE = "http://coordinator-service:5000"
 STOCK_SERVICE = "http://stock-service:5000"
 
