a16823056eedfaafc7079913d3596326a7cc24b4
Marko Matusovic <m.matusovic@student.tudelft.nl>
2022-06-22 09:54:56 +0200

    update the super-start

--- super-start.sh
+++ super-start.sh
@@ -4,14 +4,19 @@ minikube start
 minikube addons enable ingress
 
 #start tunnel
-# minikube tunnel
+minikube tunnel &
 
 # add admin role to default user such that pod can acces minikube api
 kubectl create clusterrolebinding add-on-cluster-admin2 --clusterrole=cluster-admin --serviceaccount=default:default
 
-./deploy-charts-minikube.sh
+# ./deploy-charts-minikube.sh
 
-./build_and_push_all.sh
+# ./build_and_push_all.sh
 
-cd ./k8s
-kubectl apply -f .
+kubectl apply -f ./k8s/coordinator-app.yaml
+kubectl apply -f ./k8s/ingress-service.yaml
+kubectl apply -f ./k8s/order-app.yaml
+kubectl apply -f ./k8s/order-sharding-service.yaml
+kubectl apply -f ./k8s/payment-app.yaml
+kubectl apply -f ./k8s/stock_updater.yaml
+kubectl apply -f ./k8s/stock-app.yaml
